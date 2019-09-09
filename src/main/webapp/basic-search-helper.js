f11f13f51cc46a6f87364fdecbfaabaecd1b68fc
Sam <samuelechu@umail.ucsb.edu>
2019-09-09 12:51:34 -0700

    Integrate filterTree functions into basic search

---
+++ src/main/webapp/basic-search-helper.js
@@ -0,0 +1,100 @@
+const timeProperties = [
+  'created',
+  'datetime.end',
+  'datetime.start',
+  'effective',
+  'expiration',
+  'metacard.created',
+  'metacard.modified',
+  'metacard.version.versioned-on',
+  'modified',
+]
+const datatypeProperties = ['metadata-content-type', 'datatype']
+export const fromFilterTree = filterTree => {
+  return filterTree.filters.reduce((accumulator, filter) => {
+    const { property, value, filters } = filter
+    if (property === 'anyText') {
+      accumulator.text = value
+    } else if (property === 'anyGeo') {
+      accumulator.location = { value, geojson: filter.geojson }
+    } else if (filters && filters[0]) {
+      if (timeProperties.includes(filters[0].property)) {
+        accumulator.timeRange = {
+          ...filters[0],
+          applyTo: filters.map(({ property }) => property),
+        }
+      }
+
+      if (datatypeProperties.includes(filters[0].property)) {
+        const applyTo = new Set(filters.map(({ value }) => value))
+        accumulator.datatypes = { applyTo: [...applyTo] }
+      }
+    }
+
+    return accumulator
+  }, {})
+}
+
+export const toFilterTree = basicData => {
+  const getTimeRangeFilter = () => {
+    const { applyTo, ...rest } = basicData.timeRange
+    if (!applyTo || !rest) {
+      return null
+    }
+    return {
+      type: 'OR',
+      filters: applyTo.map(property => ({
+        property,
+        ...rest,
+      })),
+    }
+  }
+
+  const getDatatypesFilter = () => {
+    const { applyTo } = basicData.datatypes
+    if (!applyTo) {
+      return null
+    }
+    const datatypeFilters = applyTo.map(value => ({
+      type: 'ILIKE',
+      property: 'datatype',
+      value,
+    }))
+    const contentTypeFilters = applyTo.map(value => ({
+      type: 'ILIKE',
+      property: 'metadata-content-type',
+      value,
+    }))
+    return {
+      type: 'OR',
+      filters: [...datatypeFilters, ...contentTypeFilters],
+    }
+  }
+
+  const text = basicData.text
+    ? {
+        type: 'ILIKE',
+        property: 'anyText',
+        value: basicData.text,
+      }
+    : null
+  const location = basicData.location
+    ? {
+        type: 'INTERSECTS',
+        property: 'anyGeo',
+        value: basicData.location.value,
+        geojson: basicData.location.geojson,
+      }
+    : null
+  const timeRange = getTimeRangeFilter()
+  const datatypes = getDatatypesFilter()
+
+  const filters = [text, location, timeRange, datatypes].filter(
+    filter => filter !== null
+  )
+
+  return {
+    type: 'AND',
+    filters,
+  }
+}
--- src/main/webapp/basic-search.js
+++ src/main/webapp/basic-search.js
@@ -4,40 +4,68 @@ import { connect } from 'react-redux'
 import Button from '@material-ui/core/Button'
 import Paper from '@material-ui/core/Paper'
 import TextField from '@material-ui/core/TextField'
+import Select from '@material-ui/core/Select'
+import FormControl from '@material-ui/core/FormControl'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+import FormLabel from '@material-ui/core/FormLabel'
+import InputLabel from '@material-ui/core/InputLabel'
+import MenuItem from '@material-ui/core/MenuItem'
 
 import { executeQuery } from './intrigue-api/lib/cache'
 
-const TextSearch = props => {
-  const [filterTree, setFilterTree] = React.useState({
-    type: 'ILIKE',
-    property: 'anyText',
-    value: '',
-  })
+import exampleFilterTree from './filterTree.json'
+import { toFilterTree, fromFilterTree } from './basic-search-helper'
+
+//console.log(fromFilterTree(exampleFilterTree));
+const TextSearch = ({ filterTree, setFilterTree }) => {
+  // const [filterTree, setFilterTree] = React.useState({
+  //   type: "ILIKE",
+  //   property: "anyText",
+  //   value: ""
+  // });
 
   return (
-    <React.Fragment>
-      <TextField
-        label="Text"
-        variant="outlined"
-        fullWidth
-        value={filterTree.value}
-        onChange={e => {
-          const value = e.target.value
-          setFilterTree({ ...filterTree, value })
-        }}
-      />
-
-      <Button
-        fullWidth
-        variant="contained"
-        color="primary"
-        onClick={() => props.onSearch(filterTree)}
-      >
-        Search
-      </Button>
-    </React.Fragment>
+    <TextField
+      label="Text"
+      variant="outlined"
+      fullWidth
+      value={filterTree.text}
+      onChange={e => {
+        const value = e.target.value
+        setFilterTree({ ...filterTree, text: value })
+      }}
+    />
   )
 }
+
+const SearchButton = props => (
+  <Button
+    fullWidth
+    variant="contained"
+    color="primary"
+    onClick={props.onSearch}
+  >
+    Search
+  </Button>
+)
+
+const TimeRange = props => (
+  <FormControl>
+    <InputLabel htmlFor="age-simple">Age</InputLabel>
+    <Select
+      value={10}
+      // onChange={(}
+      inputProps={{
+        name: 'age',
+        id: 'age-simple',
+      }}
+    >
+      <MenuItem value={10}>Ten</MenuItem>
+      <MenuItem value={20}>Twenty</MenuItem>
+      <MenuItem value={30}>Thirty</MenuItem>
+    </Select>
+  </FormControl>
+)
 
 const populateDefaultQuery = filterTree => ({
   // hello source to test error case
@@ -58,11 +86,20 @@ const populateDefaultQuery = filterTree => ({
 })
 
 export const BasicSearch = props => {
+  const [filterTree, setFilterTree] = React.useState(
+    fromFilterTree(exampleFilterTree)
+  )
+  console.log(exampleFilterTree)
+  console.log(fromFilterTree(exampleFilterTree))
+  console.log(filterTree)
+
   return (
     <Paper style={{ maxWidth: 600, margin: '20px auto' }}>
-      <TextSearch
-        onSearch={filterTree => {
-          props.onSearch(populateDefaultFilterTree(filterTree))
+      <TextSearch filterTree={filterTree} setFilterTree={setFilterTree} />
+      <TimeRange />
+      <SearchButton
+        onSearch={() => {
+          props.onSearch(populateDefaultQuery(toFilterTree(filterTree)))
         }}
       />
     </Paper>
