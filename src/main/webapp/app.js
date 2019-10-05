10d438e5fb47bcbefed7bdb500edbc885ce90c75
Chris Badahdah <a.s.badahdah@gmail.com>
2019-10-05 11:05:11 -0700

    Adds GraphQL API layer

--- src/main/webapp/app.js
+++ src/main/webapp/app.js
@@ -6,26 +6,32 @@ import { Provider } from 'react-redux'
 import createStore from './store'
 import { fetchProperties } from './store/properties'
 import { initTransport } from './intrigue-api/lib/cache'
+import { createClient } from './intrigue-api/graphql'
+import { ApolloProvider } from '@apollo/react-hooks'
 
 const store = createStore()
 store.dispatch(fetchProperties())
 store.dispatch(initTransport('http'))
 
-const render = Routes => {
+const render = (Routes, client) => {
   ReactDOM.render(
     <AppContainer>
       <Provider store={store}>
-        <Routes />
+        <ApolloProvider client={client}>
+          <Routes />
+        </ApolloProvider>
       </Provider>
     </AppContainer>,
     document.getElementById('root')
   )
 }
 
-render(Routes)
+render(Routes, createClient())
 
 if (process.env.NODE_ENV !== 'production') {
-  module.hot.accept('./routes', () => render(require('./routes').default))
+  module.hot.accept('./routes', () => {
+    render(require('./routes').default, createClient())
+  })
   module.hot.accept('./store', () => {
     const { rootReducer } = require('./store')
     store.replaceReducer(rootReducer)
@@ -33,5 +39,9 @@ if (process.env.NODE_ENV !== 'production') {
   module.hot.accept('./intrigue-api/lib/cache', () => {
     const { initTransport } = require('./intrigue-api/lib/cache')
     store.dispatch(initTransport('http'))
+  })
+  module.hot.accept('./intrigue-api/graphql', () => {
+    const { createClient } = require('./intrigue-api/graphql')
+    render(require('./routes').default, createClient())
   })
 }
