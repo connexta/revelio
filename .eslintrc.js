e2ff3105e9fcc0a26e2e564dc153bda3d817d513
Chris Badahdah <a.s.badahdah@gmail.com>
2019-10-09 17:20:51 -0700

    Adds eslint

---
+++ .eslintrc.js
@@ -0,0 +1,52 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    'react-hooks',
+    'import',
+  ],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    'import/ignore': [
+      '@connexta',
+      // TODO: Remove this
+      'intrigue-api',
+    ],
+  },
+  rules: {
+    'no-empty': [
+      'error',
+      {
+        allowEmptyCatch: true,
+      }
+    ],
+    'react/prop-types': 'off',
+    'react/display-name': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+  }
+}
