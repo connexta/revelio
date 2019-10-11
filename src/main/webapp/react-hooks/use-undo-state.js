851a000e96465edac3da74f8d79002063e654509
Chris Badahdah <a.s.badahdah@gmail.com>
2019-10-10 17:54:59 -0700

    Fixes transfer-list performance in result-forms

--- src/main/webapp/react-hooks/use-undo-state.js
+++ src/main/webapp/react-hooks/use-undo-state.js
@@ -1,8 +1,8 @@
-import { useState } from 'react'
+import { useState, useCallback } from 'react'
 import { Map, List, is } from 'immutable'
 
 const useUndoState = initialState => {
-  const [state, setState] = useState(
+  const [state, setState] = useState(() =>
     Map({
       current: 0,
       history: List([initialState]),
@@ -14,31 +14,43 @@ const useUndoState = initialState => {
 
   const currentState = history.get(current)
 
-  const setCurrentState = nextState => {
-    // prevent the same history item from being pushed to the stack twice
-    if (!is(currentState, nextState)) {
-      const update = state
-        .update('current', current => current + 1)
-        .update('history', history =>
-          history.slice(0, state.get('current') + 1).push(nextState)
-        )
+  const setCurrentState = useCallback(
+    nextState => {
+      // prevent the same history item from being pushed to the stack twice
+      if (!is(currentState, nextState)) {
+        const update = state
+          .update('current', current => current + 1)
+          .update('history', history =>
+            history.slice(0, state.get('current') + 1).push(nextState)
+          )
 
-      setState(update)
-    }
-  }
+        setState(update)
+      }
+    },
+    [state, setState, currentState]
+  )
 
   const canUndo = current > 0
-  const onUndo = () => {
-    if (canUndo) {
-      setState(state.update('current', i => i - 1))
-    }
-  }
+
+  const onUndo = useCallback(
+    () => {
+      if (canUndo) {
+        setState(state.update('current', i => i - 1))
+      }
+    },
+    [state, setState, canUndo]
+  )
+
   const canRedo = current < history.size - 1
-  const onRedo = () => {
-    if (canRedo) {
-      setState(state.update('current', i => i + 1))
-    }
-  }
+
+  const onRedo = useCallback(
+    () => {
+      if (canRedo) {
+        setState(state.update('current', i => i + 1))
+      }
+    },
+    [state, setState, canRedo]
+  )
 
   return {
     state: currentState,
