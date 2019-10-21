d50410ab3e45149ce7f1a2dcf1a8618c695f28ab
Chris Badahdah <a.s.badahdah@gmail.com>
2019-10-21 15:52:53 -0700

    Adds gen-results for storybook data generation

--- src/main/webapp/results/results.stories.js
+++ src/main/webapp/results/results.stories.js
@@ -5,6 +5,8 @@ import Skeleton from '@material-ui/lab/Skeleton'
 import { boolean } from '@connexta/ace/@storybook/addon-knobs'
 import tn from './story-thumb'
 
+import genResults from '../gen-results'
+
 const stories = storiesOf('Result', module)
 stories.addDecorator(Story => <Story />)
 
@@ -27,41 +29,10 @@ const Thumbnail = () => {
   )
 }
 
-const LA = {
-  title: 'Los Angeles',
-  description: `Painting should do one thing. It should put happiness in your heart.
-  Give him a friend, we forget the trees get lonely too. Don't forget to tell these special people in your life just how special they are to you.
-  If I paint something, I don't want to have to explain what it is. We're not trying to teach you a thing to copy.
-  We're just here to teach you a technique, then let you loose into the world. Trees get lonely too, so we'll give him a little friend.`,
-  created: 'September 4, 1781',
-  modified: 'April 29, 1992',
-  checksum: '1',
-}
-
-let results = []
-
-results.push({
-  metacard: {
-    properties: {
-      ...LA,
-      id: '-1',
-    },
-  },
-})
-
-for (let i = 0; i < 100; i++) {
-  results.push({
-    metacard: {
-      properties: {
-        ...LA,
-        id: i.toString(),
-      },
-    },
-  })
-}
-
 stories.add('Basic', () => {
   const [selection, setSelected] = useState([])
+
+  const results = genResults()
 
   return (
     <div
