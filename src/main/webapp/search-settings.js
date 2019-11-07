ef48ecd39bebb54ecbaefc28b5671a5b7873183c
gordocanchola <gordo.canchola@connexta.com>
2019-11-07 15:15:16 -0700

    INV-20 Sort Order - Multiple Sorts (#42)
    Adding the ability to sort on multiple attribute

--- src/main/webapp/search-settings.js
+++ src/main/webapp/search-settings.js
@@ -1,14 +1,22 @@
-import React, { useState } from 'react'
+import React, { useEffect } from 'react'
 import { useQuery } from '@apollo/react-hooks'
 import { useApolloFallback } from './react-hooks'
 import gql from 'graphql-tag'
 
+import Box from '@material-ui/core/Box'
+import Button from '@material-ui/core/Button'
+import Fab from '@material-ui/core/Fab'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
 import LinearProgress from '@material-ui/core/LinearProgress'
 import MenuItem from '@material-ui/core/MenuItem'
 import Paper from '@material-ui/core/Paper'
+import ReactSelect from 'react-select'
+import Remove from '@material-ui/icons/Remove'
 import Select from '@material-ui/core/Select'
 import Typography from '@material-ui/core/Typography'
-import ReactSelect from 'react-select'
+
+import { fromJS } from 'immutable'
 
 const getDirectionLabel = type => {
   let ascending = ''
@@ -63,9 +71,13 @@ const sampleAttributeDescriptors = [
 ]
 
 const AttributeSortOrder = props => {
-  const { attributeDescriptors, onChange } = props
-  const [attribute, setAttribute] = useState(attributeDescriptors[0])
-  const [direction, setDirection] = useState('ascending')
+  const { attributeDescriptors, availableDescriptors, onChange } = props
+
+  const value = fromJS(props.value || {})
+
+  const attribute = attributeDescriptors.find(
+    attr => attr.id === value.get('attribute')
+  )
 
   return (
     <div
@@ -78,21 +90,62 @@ const AttributeSortOrder = props => {
       }}
     >
       <AttributeSelect
-        attributeDescriptors={attributeDescriptors}
+        value={attribute}
+        attributeDescriptors={availableDescriptors}
         onChange={attribute => {
-          setAttribute(attribute)
-          onChange({ attribute: attribute.id, direction })
+          onChange(value.set('attribute', attribute.id))
         }}
       />
 
       <DirectionSelect
+        value={value.get('direction')}
         attributeType={attribute.type}
         onChange={direction => {
-          setDirection(direction)
-          onChange({ attribute: attribute.id, direction })
+          onChange(value.set('direction', direction))
         }}
       />
     </div>
+  )
+}
+
+const RemovableAttributeSortOrder = props => {
+  const { onRemove } = props
+
+  return (
+    <Box style={{ margin: 10 }}>
+      <Fab onClick={onRemove} size="small" color="secondary">
+        <Remove />
+      </Fab>
+    </Box>
+  )
+}
+
+const MultipleSorts = props => {
+  const { value, onChange } = props
+
+  return (
+    <List>
+      {value.map((sortItem, index) => {
+        return (
+          <ListItem key={sortItem.get('attribute')}>
+            <Box style={{ display: 'flex', alignItems: 'center' }}>
+              <RemovableAttributeSortOrder
+                onRemove={() => {
+                  onChange(value.delete(index))
+                }}
+              />
+              <AttributeSortOrder
+                {...props}
+                value={sortItem}
+                onChange={sort => {
+                  onChange(value.set(index, sort))
+                }}
+              />
+            </Box>
+          </ListItem>
+        )
+      })}
+    </List>
   )
 }
 
@@ -104,8 +157,9 @@ const createOption = option => {
 }
 
 const AttributeSelect = props => {
-  const { attributeDescriptors, onChange } = props
+  const { attributeDescriptors, value, onChange } = props
   const options = attributeDescriptors.map(createOption)
+  const defaultValue = value ? createOption(value) : options[0]
 
   const handleChange = selectedOption => {
     onChange(selectedOption.value)
@@ -113,7 +167,7 @@ const AttributeSelect = props => {
 
   return (
     <ReactSelect
-      defaultValue={options[0]}
+      defaultValue={defaultValue}
       options={options}
       onChange={handleChange}
     />
@@ -121,16 +175,14 @@ const AttributeSelect = props => {
 }
 
 const DirectionSelect = props => {
-  const { attributeType, onChange } = props
+  const { attributeType, onChange, value = 'ascending' } = props
   const directionLabel = getDirectionLabel(attributeType)
-  const [direction, setDirection] = useState('ascending')
 
   return (
     <Select
       fullWidth
-      value={direction}
+      value={value}
       onChange={e => {
-        setDirection(e.target.value)
         onChange(e.target.value)
       }}
     >
@@ -144,22 +196,85 @@ const DirectionSelect = props => {
   )
 }
 
+const sortById = (a, b) => {
+  return a.id > b.id ? 1 : -1
+}
+
+const getAvailableAttributes = (descriptors, used) => {
+  return descriptors
+    .filter(descriptor => !used.includes(descriptor.id))
+    .sort(sortById)
+}
+
+const getUsedAttributes = sorts => {
+  return sorts.map(sort => sort.get('attribute'))
+}
+
+const getNextAvailableSort = descriptors => {
+  const direction = 'ascending'
+  const attribute = descriptors[0].id
+
+  return { attribute, direction }
+}
+
 const SortOrder = props => {
-  const {
-    attributeDescriptors = sampleAttributeDescriptors,
-    setSortOrder,
-  } = props
-  const onChange = sort => {
-    setSortOrder([sort])
+  const { attributeDescriptors = sampleAttributeDescriptors } = props
+
+  const value = fromJS(props.value || [])
+
+  const onChange = value => {
+    props.onChange(value.toJS())
   }
+  const unavailableAttributes = getUsedAttributes(value)
+  const availableDescriptors = getAvailableAttributes(
+    attributeDescriptors,
+    unavailableAttributes
+  )
+
+  const pushNextSort = () => {
+    const nextSort = getNextAvailableSort(availableDescriptors)
+    return value.push(fromJS(nextSort))
+  }
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect(() => {
+    if (value.isEmpty()) {
+      onChange(pushNextSort())
+    }
+  }, [])
 
   return (
     <div>
-      <Typography variant="h6">SORT ORDER</Typography>
       <AttributeSortOrder
         attributeDescriptors={attributeDescriptors}
-        onChange={onChange}
+        availableDescriptors={availableDescriptors}
+        onChange={sort => {
+          const next = value.shift().unshift(sort)
+          onChange(next)
+        }}
+        value={value.get(0) || getNextAvailableSort(availableDescriptors)}
       />
+
+      <MultipleSorts
+        attributeDescriptors={attributeDescriptors}
+        availableDescriptors={availableDescriptors}
+        onChange={list => {
+          const next = value.slice(0, 1).concat(list)
+          onChange(next)
+        }}
+        value={value.slice(1)}
+      />
+
+      <Button
+        variant="outlined"
+        color="primary"
+        disabled={availableDescriptors.length === 0}
+        onClick={() => {
+          onChange(pushNextSort())
+        }}
+      >
+        Add Sort
+      </Button>
     </div>
   )
 }
--- src/main/webapp/search-settings.stories.js
+++ src/main/webapp/search-settings.stories.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { action } from '@connexta/ace/@storybook/addon-actions'
 import { storiesOf } from './@storybook/react'
@@ -9,6 +9,28 @@ const stories = storiesOf('Search Settings', module)
 
 stories.addDecorator(Story => <Story />)
 
+const useSortState = (sorts = []) => {
+  const [value, setState] = useState(sorts)
+
+  const onChange = (...args) => {
+    action('onChange')(...args)
+    setState(...args)
+  }
+
+  return { value, onChange }
+}
+
 stories.add('Sort Order', () => {
-  return <SortOrder setSortOrder={action('setSortOrder')} />
+  return <SortOrder {...useSortState()} />
+})
+
+stories.add('Sort Order with values provided', () => {
+  const sorts = [
+    { attribute: 'thumbnail', direction: 'ascending' },
+    { attribute: 'phonetics', direction: 'descending' },
+    { attribute: 'media.bit-rate', direction: 'ascending' },
+    { attribute: 'location', direction: 'descending' },
+  ]
+
+  return <SortOrder {...useSortState(sorts)} />
 })
