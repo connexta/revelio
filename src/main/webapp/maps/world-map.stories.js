5c4df81210c8c0f6de614684253f5523f08c0804
Bernard Igiri <bernard.igiri@connexta.com>
2019-11-08 16:21:45 -0500

    Fixed path errors.

--- src/main/webapp/maps/world-map.stories.js
+++ src/main/webapp/maps/world-map.stories.js
@@ -1,4 +1,4 @@
-import { storiesOf } from './@storybook/react'
+import { storiesOf } from '../@storybook/react'
 import { withKnobs, select } from '@connexta/ace/@storybook/addon-knobs'
 import React from 'react'
 import WorldMap from './world-map'
