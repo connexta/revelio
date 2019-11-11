df6c25a9e01fbc76bb1d755f5f88fa57eb38f93d
Chris Badahdah <a.s.badahdah@gmail.com>
2019-11-11 13:07:27 -0700

    Updates navigation to match DDF

--- src/main/webapp/routes.js
+++ src/main/webapp/routes.js
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+
+import { getIn } from 'immutable'
 
 import AppBar from '@material-ui/core/AppBar'
 import Button from '@material-ui/core/Button'
@@ -80,13 +82,7 @@ const createRoute = (path, title, Icon = AccessibleForwardIcon, component) => {
 }
 
 const routes = [
-  createRoute('/', 'Home', HomeIcon),
-  createRoute(
-    '/workspaces',
-    'Workspaces',
-    CollectionsBookmarkIcon,
-    WorkspacesIndex
-  ),
+  createRoute('/', 'Workspaces', CollectionsBookmarkIcon, WorkspacesIndex),
   createRoute('/search', 'Search', SearchIcon, SimpleSearch),
   createRoute('/sources', 'Sources', CloudIcon, SourcesRoute),
   createRoute('/search-forms', 'Search Forms', FindInPageIcon),
@@ -131,24 +127,50 @@ const query = gql`
   }
 `
 
-const NavMenuBranding = () => {
-  const { loading, error, data } = useQuery(query)
+const NavMenu = props => {
+  const { data } = useQuery(query)
 
-  if (loading || error) {
-    return null
-  }
+  const branding = getIn(data, ['systemProperties', 'branding'], '')
+  const product = getIn(data, ['systemProperties', 'product'], '')
 
-  const { branding, product } = data.systemProperties
+  const { routes, onClose } = props
 
   return (
-    <List>
+    <List style={{ width: 300 }}>
+      {branding !== '' ? (
+        <Fragment>
+          <ListItem>
+            <ListItemText
+              primaryTypographyProps={{ style: { fontSize: '2rem' } }}
+              secondaryTypographyProps={{ style: { fontSize: '1.3rem' } }}
+              primary={branding}
+              secondary={product}
+            />
+          </ListItem>
+          <Divider />
+        </Fragment>
+      ) : null}
+      {routes.map(route => {
+        const { path, link: Link } = route
+        return (
+          <ListItem key={path}>
+            <Link onClick={onClose} />
+          </ListItem>
+        )
+      })}
+      <Divider />
       <ListItem>
-        <ListItemText
-          primaryTypographyProps={{ style: { fontSize: '2rem' } }}
-          secondaryTypographyProps={{ style: { fontSize: '1.3rem' } }}
-          primary={branding}
-          secondary={product}
-        />
+        <Button
+          fullWidth
+          href="/"
+          component="a"
+          style={{ justifyContent: 'flex-start' }}
+        >
+          <ListItemIcon>
+            <HomeIcon style={{ marginRight: 10 }} />
+          </ListItemIcon>
+          <ListItemText primary={`${branding || ''} Home`} />
+        </Button>
       </ListItem>
     </List>
   )
@@ -176,18 +198,7 @@ const AppRouter = () => {
     >
       <Router>
         <Drawer anchor="left" open={open} onClose={handleDrawerClose}>
-          <NavMenuBranding />
-          <Divider />
-          <List style={{ width: 300 }}>
-            {routes.map(route => {
-              const { path, link: Link } = route
-              return (
-                <ListItem key={path}>
-                  <Link onClick={handleDrawerClose} />
-                </ListItem>
-              )
-            })}
-          </List>
+          <NavMenu routes={routes} onClose={handleDrawerClose} />
         </Drawer>
         {routes.concat(otherRoutes).map(route => {
           const { title, path, component: Component } = route
