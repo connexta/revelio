063304ade494ad3e086e9a2e3e37234aa0898706
willwill96 <william.will@connexta.com>
2019-11-22 10:35:16 -0700

    Text Filter to provide suggestions

--- src/main/webapp/query-filters/filter-input/text-filter.tsx
+++ src/main/webapp/query-filters/filter-input/text-filter.tsx
@@ -1,7 +1,19 @@
 import * as React from 'react'
 import { QueryFilterProps } from '../filter/individual-filter'
-import { TextField, Box } from '@material-ui/core'
-import { Map } from 'immutable'
+import {
+  TextField,
+  Box,
+  Paper,
+  LinearProgress,
+  Typography,
+} from '@material-ui/core'
+import { Map, getIn, List } from 'immutable'
+import { useFilterContext } from '../filter-context'
+import Autocomplete from '@material-ui/lab/Autocomplete'
+import { useQuery } from '@apollo/react-hooks'
+import gql from 'graphql-tag'
+const useApolloFallback = require('../../react-hooks/use-apollo-fallback')
+  .default
 
 export const comparatorOptions = ['ILIKE', 'LIKE', '=', 'NEAR', 'IS NULL']
 export const comparatorAliases = Map({
@@ -18,21 +30,86 @@ const validateText = (value: any) => {
   return errors
 }
 
-const TextFilter = (props: QueryFilterProps) => {
-  const errors = validateText(props.value)
+const FACETED_QUERY = gql`
+  query getFactedOptions($attribute: String!) {
+    facet(attribute: $attribute) {
+      value
+    }
+  }
+`
+const Loading = () => {
   return (
-    <TextField
-      error={errors.value !== undefined}
-      helperText={errors.value}
-      placeholder="Use * for wildcard"
-      variant="outlined"
-      fullWidth
-      onChange={event => {
+    <Paper>
+      <LinearProgress />
+    </Paper>
+  )
+}
+const Error = (props: any) => {
+  return (
+    <Paper>
+      <Typography>
+        {props.message ? props.message : 'Something went wrong'}
+      </Typography>
+    </Paper>
+  )
+}
+
+const TextFilterContainer = (props: QueryFilterProps) => {
+  const { data, loading, error } = useQuery(FACETED_QUERY, {
+    variables: { attribute: props.property },
+  })
+  if (loading) {
+    return <Loading />
+  }
+
+  if (error) {
+    return <Error message={error} />
+  }
+  return (
+    <TextFilter
+      {...props}
+      enums={getIn(data, ['facet'], []).map((facet: any) => facet.value)}
+    />
+  )
+}
+
+type TextFilterProps = QueryFilterProps & {
+  enums?: string[]
+}
+
+const TextFilter = (props: TextFilterProps) => {
+  const { metacardTypes } = useFilterContext()
+  const { enums = [] } = props
+  const errors = validateText(props.value)
+
+  const getOptions = () => {
+    return List(
+      enums.concat(getIn(metacardTypes, [props.property, 'enums'], []))
+    )
+      .toSet()
+      .toArray()
+  }
+
+  return (
+    <Autocomplete
+      freeSolo
+      disableClearable
+      options={getOptions()}
+      value={props.value}
+      onChange={(_, value: any) => {
         const { property, type } = props
-        const value = event.target.value
         props.onChange({ property, type, value })
       }}
-      value={props.value}
+      renderInput={params => (
+        <TextField
+          {...params}
+          error={errors.value !== undefined}
+          helperText={errors.value}
+          placeholder="Use * for wildcard"
+          variant="outlined"
+          fullWidth
+        />
+      )}
     />
   )
 }
@@ -96,14 +173,11 @@ const NearFilter = (props: QueryFilterProps) => {
 }
 
 const Filter = (props: QueryFilterProps) => {
-  return (
-    <React.Fragment>
-      {props.type !== 'NEAR' ? (
-        <TextFilter {...props} />
-      ) : (
-        <NearFilter {...props} />
-      )}
-    </React.Fragment>
-  )
+  if (props.type === 'NEAR') {
+    return <NearFilter {...props} />
+  }
+
+  const Component = useApolloFallback(TextFilterContainer, TextFilter)
+  return <Component {...props} />
 }
 export default Filter
