4471adaab1a6bb031068d02af8c6bd580b992e4d
gjvera <git@gabrieljosephvera.com>
2019-12-06 13:34:13 -0700

    Remove unused import

--- src/main/webapp/login/login.stories.js
+++ src/main/webapp/login/login.stories.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { storiesOf } from '../@storybook/react'
 const stories = storiesOf('LogIn', module)
 import { LogIn } from './login'
