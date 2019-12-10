fb6849e6451cea9c9ca44e4414ffdb19155faf5c
Ryan Schachte <code@ryan-schachte.com>
2019-12-10 16:33:35 -0700

    Updating script logic

--- dockerDeploy.sh
+++ dockerDeploy.sh
@@ -3,14 +3,14 @@ echo "Logging into Docker registry"
 
 docker login -u $DOCKER_HUB_USER -p $DOCKER_HUB_PASSWORD
 
-echo "Tagging current image"
+echo "Tagging current image to push into $DOCKER_REPO_URL"
 
 currentHash=$(git rev-parse --short=8 HEAD)
-imageName="benandryan1/revelio2:$currentHash"
+imageName="$DOCKER_REPO_URL:$currentHash"
 
 echo "Tagging image as: $imageName"
 
 docker build -t $imageName .
 docker push $imageName
 
-echo "Image push complete"
+echo "Image push complete and viewable at https://hub.docker.com/repository/docker/$DOCKER_REPO_URL"
