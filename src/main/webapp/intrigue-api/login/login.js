2972227a18277da3225821d7cfb107d11ee859df
gjvera <git@gabrieljosephvera.com>
2019-12-12 12:01:20 -0700

    Moving resolver and mutation to separate file

---
+++ src/main/webapp/intrigue-api/login/login.js
@@ -0,0 +1,33 @@
+const ROOT = '/search/catalog/internal'
+
+const typeDefs = `
+    extend type Mutation {
+        logIn(username: String!, password: String!): String
+    }`
+
+const btoa = arg => {
+  if (typeof window !== 'undefined') {
+    return window.btoa(arg)
+  }
+  return Buffer.from(arg).toString('base64')
+}
+
+const logIn = async (parent, args, { fetch }) => {
+  const { username, password } = args
+  const Authorization = 'Basic ' + btoa(`${username}:${password}`)
+  const res = await fetch(ROOT, {
+    Authorization,
+  })
+  return res.headers.get('set-cookie')
+}
+
+const resolvers = {
+  Mutation: {
+    logIn,
+  },
+}
+
+module.exports = {
+  resolvers,
+  typeDefs,
+}
