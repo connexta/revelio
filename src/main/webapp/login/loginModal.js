04886c391a10dc1f5b05857e7432bbc97df44fab
gjvera <git@gabrieljosephvera.com>
2019-12-18 17:15:29 -0700

    Updating schema for new mutation

--- src/main/webapp/login/loginModal.js
+++ src/main/webapp/login/loginModal.js
@@ -3,10 +3,10 @@ import Dialog from '@material-ui/core/Dialog'
 import LogIn from './login'
 
 export const LogInModal = props => {
-  const { handleClose, open } = props
+  const { handleClose, open, label } = props
   return (
     <Dialog open={open} onClose={handleClose}>
-      <LogIn handleClose={handleClose} />
+      <LogIn handleClose={handleClose} label={label} />
     </Dialog>
   )
 }
