8d39bf4a2a788e05a80cd7a9ce885084ea06f2a7
samuelechu <samuelechu@gmail.com>
2020-01-31 11:16:18 -0700

    Adds ability to create workspaces (#193)

--- src/main/webapp/workspaces/workspaces.js
+++ src/main/webapp/workspaces/workspaces.js
@@ -11,6 +11,7 @@ import Tabs from '@material-ui/core/Tabs'
 import {
   IndexCards,
   IndexCardItem,
+  AddCardItem,
   DeleteAction,
   ShareAction,
   Actions,
@@ -18,12 +19,12 @@ import {
 
 import RetryNotification from '../retry/retry'
 
-import { Link, useParams } from 'react-router-dom'
+import { Link, useParams, Redirect } from 'react-router-dom'
 
 import { useQueryExecutor } from '../react-hooks'
 
 import gql from 'graphql-tag'
-import { useQuery } from '@apollo/react-hooks'
+import { useQuery, useMutation } from '@apollo/react-hooks'
 
 import QueryStatus from '../query-status'
 import { BasicSearch, populateDefaultQuery } from '../basic-search'
@@ -229,6 +230,31 @@ export const Workspace = () => {
   )
 }
 
+const Workspaces = props => {
+  const { workspaces, onCreate } = props
+
+  return (
+    <IndexCards>
+      <AddCardItem onClick={onCreate} />
+      {workspaces.map(workspace => {
+        return (
+          <Link
+            key={workspace.id}
+            to={`/workspaces/${workspace.id}`}
+            style={{ textDecoration: 'none' }}
+          >
+            <IndexCardItem {...workspace}>
+              <Actions>
+                <ShareAction />
+                <DeleteAction />
+              </Actions>
+            </IndexCardItem>
+          </Link>
+        )
+      })}
+    </IndexCards>
+  )
+}
 const workspaces = gql`
   query Workspaces {
     metacardsByTag(tag: "workspace") {
@@ -241,9 +267,54 @@ const workspaces = gql`
     }
   }
 `
+const workspaceAttributes = gql`
+  fragment WorkspaceAttributes on MetacardAttributes {
+    title
+    metacard_tags
+    metacard_type
+  }
+`
+
+const useCreate = () => {
+  const [redirectId, setRedirectId] = React.useState(null)
+  const mutation = gql`
+    mutation CreateWorkspace($attrs: MetacardAttributesInput!) {
+      createMetacard(attrs: $attrs) {
+        ...WorkspaceAttributes
+        id: id
+        owner: metacard_owner
+        modified: metacard_modified
+      }
+    }
+    ${workspaceAttributes}
+  `
+
+  const [create] = useMutation(mutation, {
+    update: (cache, { data }) => {
+      const query = workspaces
+      const { metacardsByTag } = cache.readQuery({ query })
+      const updatedWorkspaces = [
+        data.createMetacard,
+        ...metacardsByTag.attributes,
+      ]
+      cache.writeQuery({
+        query,
+        data: {
+          metacardsByTag: {
+            attributes: updatedWorkspaces,
+            __typename: 'QueryResponse',
+          },
+        },
+      })
+      setRedirectId(data.createMetacard.id)
+    },
+  })
+  return [create, redirectId]
+}
 
 export default () => {
   const { refetch, loading, error, data } = useQuery(workspaces)
+  const [create, redirectId] = useCreate()
 
   if (loading) {
     return <LoadingComponent />
@@ -258,26 +329,24 @@ export default () => {
     )
   }
 
-  const attributes = data.metacardsByTag.attributes
+  if (redirectId) {
+    return <Redirect push to={`/workspaces/${redirectId}`} />
+  }
 
-  return (
-    <IndexCards>
-      {attributes.map(attrs => {
-        return (
-          <Link
-            key={attrs.id}
-            to={`/workspaces/${attrs.id}`}
-            style={{ textDecoration: 'none' }}
-          >
-            <IndexCardItem {...attrs}>
-              <Actions>
-                <ShareAction />
-                <DeleteAction />
-              </Actions>
-            </IndexCardItem>
-          </Link>
-        )
-      })}
-    </IndexCards>
+  const onCreate = () => {
+    create({
+      variables: {
+        attrs: {
+          title: 'New Workspace',
+          metacard_type: 'workspace',
+          metacard_tags: ['workspace'],
+        },
+      },
+    })
+  }
+
+  const workspacesSortedByTime = data.metacardsByTag.attributes.sort(
+    (a, b) => (a.modified > b.modified ? -1 : 1)
   )
+  return <Workspaces workspaces={workspacesSortedByTime} onCreate={onCreate} />
 }
