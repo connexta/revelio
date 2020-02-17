0739b8ee758b1f80b9089f52a9720a2268b7d887
willwill96 <william.will@connexta.com>
2020-02-17 09:19:49 -0700

    Make createServerApollo Reusable

--- src/main/webapp/intrigue-api/graphql.js
+++ src/main/webapp/intrigue-api/graphql.js
@@ -1,16 +1,7 @@
 import { SchemaLink } from 'apollo-link-schema'
-// import { makeExecutableSchema } from 'graphql-tools'
 import { ApolloClient } from 'apollo-client'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { ApolloLink } from 'apollo-link'
-// import schema from './schema'
-
-// const { resolvers, typeDefs, context } = schema
-
-// const executableSchema = makeExecutableSchema({
-//   typeDefs,
-//   resolvers,
-// })
 
 export const createServerApollo = ({ executableSchema, context }) => (
   ...args
