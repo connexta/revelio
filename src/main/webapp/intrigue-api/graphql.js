05627765aaf4488cb27228d997bd2d7f8abfaf28
willwill96 <william.will@connexta.com>
2020-02-19 14:21:48 -0700

    formatting

--- src/main/webapp/intrigue-api/graphql.js
+++ src/main/webapp/intrigue-api/graphql.js
@@ -3,9 +3,7 @@ import { ApolloClient } from 'apollo-client'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { ApolloLink } from 'apollo-link'
 
-const createServerApollo = ({ executableSchema, context }) => (
-  ...args
-) => {
+const createServerApollo = ({ executableSchema, context }) => (...args) => {
   const cache = new InMemoryCache()
   return new ApolloClient({
     link: ApolloLink.from([
