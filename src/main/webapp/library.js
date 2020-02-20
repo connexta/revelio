3b5cf7fb632ea020d1c1d316fe8588ce57ea6819
willwill96 <william.will@connexta.com>
2020-02-20 10:31:22 -0700

    Add Library Command From Ace

---
+++ src/main/webapp/library.js
@@ -0,0 +1,22 @@
+import { Sources } from './sources'
+import Workspaces, { Workspace } from './workspaces/workspaces'
+import SimpleSearch from './simple-search'
+import Search from './search/search'
+import SearchResults from './search/results'
+import SearchDetails from './search/details'
+import ResultForms from './result-forms'
+import SearchForms from './search-forms'
+import { About } from './about'
+
+export {
+  About,
+  Sources,
+  Workspace,
+  Workspaces,
+  SimpleSearch,
+  Search,
+  SearchResults,
+  SearchDetails,
+  ResultForms,
+  SearchForms,
+}
