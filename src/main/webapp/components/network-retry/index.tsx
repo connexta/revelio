30eac8127c5e5a8757ddcd7cf0072abf23e4a13c
willwill96 <william.will@connexta.com>
2020-02-26 12:30:22 -0700

    Fix import

--- src/main/webapp/components/network-retry/index.tsx
+++ src/main/webapp/components/network-retry/index.tsx
@@ -1,4 +1,4 @@
 import InlineRetry from './inline-retry'
-const SnackbarRetry = require('./snackbar-retry')
+const SnackbarRetry = require('./snackbar-retry').default
 
 export { InlineRetry, SnackbarRetry }
