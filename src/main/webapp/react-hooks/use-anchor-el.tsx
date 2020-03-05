c4ee1a3d5241a79a82190712050b5f40a31135ea
samuelechu <samuelechu@gmail.com>
2020-03-04 17:24:23 -0700

    Extract header section from query builder (#270)

--- src/main/webapp/react-hooks/use-anchor-el.tsx
+++ src/main/webapp/react-hooks/use-anchor-el.tsx
@@ -1,15 +1,24 @@
 import { useState } from 'react'
 
+type UseAnchorElProps = {
+  onClose?: () => void
+}
+
 //Used as shorthand for anchorEl logic on Material UI Menus/Popovers
-const useAnchorEl = () => {
+const useAnchorEl = (props: UseAnchorElProps = {}) => {
   const [anchorEl, setAnchorEl] = useState(null)
   const open: any = (event: any) => {
     setAnchorEl(event.currentTarget)
   }
   const close = () => {
     setAnchorEl(null)
+    if (props.onClose) {
+      props.onClose()
+    }
   }
-  return [anchorEl, open, close]
+
+  const isOpen = Boolean(anchorEl)
+  return [anchorEl, open, close, isOpen]
 }
 
 export default useAnchorEl
