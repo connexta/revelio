7534af8f2e1700becc1ceee663a0d3d45a48a67c
Sam Chu <samuelechu@gmail.com>
2020-04-03 09:33:16 -0700

    Address comments

--- src/main/webapp/components/text-search/text-search.tsx
+++ src/main/webapp/components/text-search/text-search.tsx
@@ -1,9 +1,8 @@
-import Divider from '@material-ui/core/Divider'
 import Paper from '@material-ui/core/Paper'
 import TextField from '@material-ui/core/TextField'
+import { getIn } from 'immutable'
 import * as React from 'react'
 import { QueryBuilderProps } from '../query-builder/query-builder'
-import { QueryFilter } from '../query-builder/types'
 
 type TextSearchQueryBuilderProps = {
   text: string
@@ -37,9 +36,7 @@ const getTextQuery = (text: string) => ({
 
 export default (props: QueryBuilderProps) => {
   const [text, setText] = React.useState(
-    props.query && props.query.filterTree
-      ? (props.query.filterTree.filters[0] as QueryFilter).value
-      : '*'
+    getIn(props, ['query', 'filterTree', 'filters'], [{ value: '*' }])[0].value
   )
 
   const onChange = (text: string) => {
@@ -48,19 +45,15 @@ export default (props: QueryBuilderProps) => {
   }
 
   return (
-    <React.Fragment>
-      <Paper
-        style={{
-          display: 'flex',
-          alignItems: 'center',
-          padding: 16,
-          boxSizing: 'border-box',
-        }}
-      >
-        <TextSearchQueryBuilder text={text} onChange={onChange} />
-      </Paper>
-
-      <Divider />
-    </React.Fragment>
+    <Paper
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        padding: 16,
+        boxSizing: 'border-box',
+      }}
+    >
+      <TextSearchQueryBuilder text={text} onChange={onChange} />
+    </Paper>
   )
 }
