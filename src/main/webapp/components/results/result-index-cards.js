eaa50803577ecfdceff05865c9384f336f66dbb7
gjvera <git@gabrieljosephvera.com>
2020-04-16 21:56:23 -0700

    Starting work on bulk export

--- src/main/webapp/components/results/result-index-cards.js
+++ src/main/webapp/components/results/result-index-cards.js
@@ -8,11 +8,22 @@ import useAnchorEl from '../../react-hooks/use-anchor-el'
 import { ResultListInteraction } from '../lists/result-list-interaction'
 import { ExportAction } from '../result-export/result-export-action'
 import Divider from '@material-ui/core/Divider'
+import Checkbox from '@material-ui/core/Checkbox'
 
 export default props => {
   const { results, setLists, lists } = props
   const [selectedResult, setSelectedResult] = React.useState(null)
+  const [selectedResults, setSelectedResults] = React.useState([])
   const [anchorEl, handleOpen, handleClose, isOpen] = useAnchorEl()
+
+  const modifySelectedResults = resultId => {
+    const index = selectedResults.indexOf(resultId)
+    if (index === -1) {
+      setSelectedResults([...selectedResults, resultId])
+    } else {
+      selectedResults.splice(index, 1)
+    }
+  }
 
   return (
     <React.Fragment>
@@ -30,6 +41,13 @@ export default props => {
               <Divider />
               <Actions disableSpacing={true}>
                 <ExportAction result={metacard} />
+                <Checkbox
+                  style={{ marginRight: '50%' }}
+                  size="small"
+                  onChange={() => {
+                    modifySelectedResults(metacard.attributes.id)
+                  }}
+                />
                 <IconButton
                   onClick={e => {
                     setSelectedResult(metacard.attributes.id)
