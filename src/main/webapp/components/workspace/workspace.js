49ea2a6dc3b5334208a20866987660c98998cb32
willwill96 <william.will@connexta.com>
2020-04-20 14:45:24 -0600

    fix query-status re-run

--- src/main/webapp/components/workspace/workspace.js
+++ src/main/webapp/components/workspace/workspace.js
@@ -199,12 +199,14 @@ export default () => {
 
                 <QueryStatus
                   sources={status}
-                  onRun={srcs => {
+                  onRun={sources => {
                     //setPageIndex(0)
-                    onSearch({
-                      ...queries.find(query => query.id === currentQuery),
-                      srcs,
-                    })
+                    onSearch(
+                      queryToSearch({
+                        ...queries.find(query => query.id === currentQuery),
+                        sources,
+                      })
+                    )
                   }}
                   onCancel={srcs => {
                     srcs.forEach(src => {
